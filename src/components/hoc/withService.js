87afff4bc8a47e28e237e7b527b93111161181a9
svyatoslav.oprysko <svyatoslav.oprysko@eleks.com>
2019-04-09 12:29:45 +0300

    Added service context.

---
+++ src/components/hoc/withService.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ServiceContextConsumer } from '../service-context/ServiceContext';
+
+const withService = () => (Wrapped) => {
+
+    return (props) => {
+        return (
+            <ServiceContextConsumer>
+                {
+                    (service) => {
+                        return (
+                            <Wrapped {...props} service={service}/>
+                        )
+                    }
+                }
+            </ServiceContextConsumer>
+        )
+    }
+};
+
+export default withService;
